84690fcf678d0c0061993cfb83d12d4b710f4e91
manel-91 <132007674+manel-91@users.noreply.github.com>
2023-05-01 11:19:21 -0400

    Create pgmsomme.cs

---
+++ pgmsomme.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10; // définir la valeur de n
+        int sum = 0; // initialiser la somme à zéro
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i; // ajouter chaque nombre à la somme
+        }
+
+        Console.WriteLine("La somme des nombres de 1 à {0} est : {1}", n, sum);
+        Console.ReadLine();
+    }
+}
